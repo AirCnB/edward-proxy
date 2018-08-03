d564046546320f6bcddef4fc7cf6ee4b3e07f1c7
edwardbaeg <edwardbaeg9@gmail.com>
2018-08-03 16:05:16 -0700

    Redirects reviews GET request

--- server/index.js
+++ server/index.js
@@ -4,8 +4,6 @@ const axios = require('axios');
 const app = express();
 const port = 3000;
 
-const description_url = 'http://ec2-18-222-220-204.us-east-2.compute.amazonaws.com/api/house/listings/80/';
-
 app.use('/listings/:id', express.static('public'));
 
 app.get('/api/listings/:id/photos', (req, res) => {
@@ -13,7 +11,7 @@ app.get('/api/listings/:id/photos', (req, res) => {
   const url = `http://ec2-13-59-102-97.us-east-2.compute.amazonaws.com/api/listings/${id}/photos`;
   axios.get(url)
     .then(({ data }) => res.status(200).send(data))
-    .catch(err => res.status(404).send(err));
+    .catch(err => res.status(404).send('err'));
 });
 
 app.get('/api/listings/:id/bookings', (req, res) => {
@@ -21,14 +19,22 @@ app.get('/api/listings/:id/bookings', (req, res) => {
   const url = `http://ec2-34-203-218-92.compute-1.amazonaws.com/api/listings/${id}/bookings`;
   axios.get(url)
     .then(({ data }) => res.status(200).send(data))
-    .catch(err => res.status(404).send(err));
+    .catch(err => res.status(404).send('err'));
 });
 
 app.get('/api/house/:id', (req, res) => {
   const { id } = req.params;
   const url = `http://ec2-18-222-220-204.us-east-2.compute.amazonaws.com/api/house/${id}`;
   axios.get(url)
-    .then((data) => res.status(200).send(data.data))
+    .then(({ data }) => res.status(200).send(data))
+    .catch(err => res.status(404).send('err'));
+});
+
+app.get('/:id/reviews', (req, res) => {
+  const { id } = req.params;
+  const url = `http://ec2-52-87-204-118.compute-1.amazonaws.com/${id}/reviews`;
+  axios.get(url)
+    .then(({ data }) => res.status(200).send(data))
     .catch(err => res.status(404).send('err'));
 });
 
